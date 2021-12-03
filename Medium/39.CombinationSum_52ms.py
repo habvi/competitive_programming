2e5557be7a3a752e24b3cc5f82cea2eb779c444e
habvi <a2231250601h@yahoo.co.jp>
2021-12-03 16:44:39 +0900

    update: 39

--- Medium/39.CombinationSum_52ms.py
+++ Medium/39.CombinationSum_52ms.py
@@ -1,5 +1,5 @@
-# Runtime: 56 ms, faster than 89.91% of Python3 online submissions for Combination Sum.
-# Memory Usage: 14.9 MB, less than 6.15% of Python3 online submissions for Combination Sum.
+# Runtime: 52 ms, faster than 94.76% of Python3 online submissions for Combination Sum.
+# Memory Usage: 14.7 MB, less than 8.66% of Python3 online submissions for Combination Sum.
 
 class Solution:
     def combinationSum(self, candidates, target: int):
@@ -8,9 +8,10 @@ class Solution:
         dp[0].add((0,))
         for i in range(n):
             for j in range(target):
-                if j + candidates[i] <= target:
-                    for t in dp[j]:
-                        dp[j + candidates[i]].add(t + (candidates[i],))
+                if j + candidates[i] > target:
+                    continue
+                for t in dp[j]:
+                    dp[j + candidates[i]].add(t + (candidates[i],))
         ans = []
         for a in dp[target]:
             ans.append(list(a[1:]))
