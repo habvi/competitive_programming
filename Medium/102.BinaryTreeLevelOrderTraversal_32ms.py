5fc4ed146b5d70a6962563c36daef8c91a4494a8
habvi <a2231250601h@yahoo.co.jp>
2021-12-19 17:13:22 +0900

    update: 102

--- Medium/102.BinaryTreeLevelOrderTraversal_32ms.py
+++ Medium/102.BinaryTreeLevelOrderTraversal_32ms.py
@@ -15,11 +15,7 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode], dist=0) -> List[List[int]]:
         if not root:
             return
-        
         self.lis[dist].append(root.val)
-        
-        if root.left:
-            self.levelOrder(root.left, dist + 1)
-        if root.right:
-            self.levelOrder(root.right, dist + 1)
+        self.levelOrder(root.left, dist + 1)
+        self.levelOrder(root.right, dist + 1)
         return self.lis.values()
