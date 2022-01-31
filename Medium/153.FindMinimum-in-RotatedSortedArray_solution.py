9d9b5b860e89f8fe2cbcb2ae59bd3e3c4fcce04f
habvi <a2231250601h@yahoo.co.jp>
2022-01-31 12:09:58 +0900

    add: 153_solution ver

---
+++ Medium/153.FindMinimum-in-RotatedSortedArray_solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMin(self, nums) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        if nums[right] > nums[0]:
+            return nums[0]        
+        
+        while right >= left:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            
+            if nums[mid] > nums[0]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+
+S = Solution()
+nums = [4,5,6,7,0,1,2]
+nums = [3,4,5,1,2]
+nums = [2, 1]
+print(S.findMin(nums))
