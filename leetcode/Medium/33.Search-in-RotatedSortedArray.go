3edf5808ee892472815caa17ad3690e7d2a24d66
habvi <a2231250601h@yahoo.co.jp>
2022-12-20 01:48:06 +0900

    refactor: use sort.Search

--- leetcode/Medium/33.Search-in-RotatedSortedArray.go
+++ leetcode/Medium/33.Search-in-RotatedSortedArray.go
@@ -2,44 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"sort"
 )
 
-// 4ms
-
-func abs(x int) int {
-	return int(math.Abs(float64(x)))
-}
-
-func is_ok(nums *[]int, right int) bool {
-	return (*nums)[0] > (*nums)[right]
-}
-
-func bisect(nums *[]int, ng, ok int) int {
-	for abs(ok-ng) > 1 {
-		middle := (ok + ng) / 2
-		if is_ok(nums, middle) {
-			ok = middle
-		} else {
-			ng = middle
-		}
-	}
-	return ok
-}
+// 4sms
 
 func search(nums []int, target int) int {
 	length := len(nums)
-	start := bisect(&nums, -1, length)
-	i := sort.Search(start, func(i int) bool { return nums[i] >= target })
-	if i < start && nums[i] == target {
-		return i
+	start := sort.Search(length, func(r int) bool { return nums[0] > nums[r] })
+	idx := sort.Search(start, func(i int) bool { return nums[i] >= target })
+	if idx < start && nums[idx] == target {
+		return idx
 	}
-	i = sort.Search(len(nums)-start, func(i int) bool { return nums[start+i] >= target })
-	if i < len(nums)-start && nums[start+i] == target {
-		return start + i
+	length -= start
+	idx = sort.Search(length, func(i int) bool { return nums[start+i] >= target })
+	if idx < length && nums[start+idx] == target {
+		return start + idx
 	}
-	return 0
+	return -1
 }
 
 func main() {
