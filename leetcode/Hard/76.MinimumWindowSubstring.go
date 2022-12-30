cc5deeff956ba4896d87bf520aea48da17c750ac
habvi <a2231250601h@yahoo.co.jp>
2022-12-31 03:14:51 +0900

    add: 76.go O(52*NlogN) ?

---
+++ leetcode/Hard/76.MinimumWindowSubstring.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func min(x, y int) int {
+	return int(math.Min(float64(x), float64(y)))
+}
+
+func calc_each_len_t(str string) map[byte]int {
+	res := make(map[byte]int)
+	for _, s := range str {
+		res[byte(s)] += 1
+	}
+	return res
+}
+
+func is_same_map(nums, each_len_t map[byte]int) bool {
+	ok := true
+	for k, v := range each_len_t {
+		if nums[k] < v {
+			ok = false
+			break
+		}
+	}
+	return ok
+}
+
+func minWindow(s string, t string) string {
+	nums := make(map[byte]int)
+	len_s := len(s)
+	min_len := 100005
+	var r, ans_l, ans_r int
+	each_len_t := calc_each_len_t(t)
+	for l, _ := range s {
+		for r < len_s && is_same_map(nums, each_len_t) == false {
+			nums[s[r]] += 1
+			r++
+		}
+		len_nums := r - l
+		if is_same_map(nums, each_len_t) == true && len_nums < min_len {
+			ans_l, ans_r = l, r
+			min_len = len_nums
+		}
+		nums[s[l]] -= 1
+		if nums[s[l]] == 0 {
+			delete(nums, s[l])
+		}
+	}
+	return s[ans_l:ans_r]
+}
+
+func main() {
+	s := "ADOBECODEBANC"
+	t := "ABC"
+	fmt.Println(minWindow(s, t))
+}
