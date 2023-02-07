b5a4b5602f3d46e6dc497bcf7552a9ead127f511
habvi <a2231250601h@yahoo.co.jp>
2023-02-07 19:57:12 +0900

    add: 268.go

---
+++ leetcode/Easy/268.MissingNumber.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func missingNumber(nums []int) int {
+	size := len(nums)
+	var extra_space int
+	for i := 0; i < size; i++ {
+		j := nums[i]
+		if j == size {
+			extra_space = -1
+			continue
+		}
+		for 0 <= j && j < size {
+			tmp := nums[j]
+			nums[j] = -1
+			j = tmp
+		}
+	}
+
+	for i := 0; i < size; i++ {
+		if nums[i] >= 0 {
+			return i
+		}
+	}
+	fmt.Println(extra_space) // how golang (void)extra_space
+	return size
+}
+
+func main() {
+	nums := []int{3, 0, 1}
+	fmt.Println(missingNumber(nums))
+	nums = []int{9, 6, 4, 2, 3, 5, 7, 0, 1}
+	fmt.Println(missingNumber(nums))
+	nums = []int{1, 2}
+	fmt.Println(missingNumber(nums))
+}
